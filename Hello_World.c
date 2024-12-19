b09f023707e62fc8d855e9f50fc669338a5aaeb6
Manish Murmu <murmu.manish@proton.me>
2024-12-19 21:43:49 +0530

    Updated Hello World

--- Hello_World.c
+++ Hello_World.c
@@ -1,9 +1,18 @@
+//stdio.h is a Standard Input/Output Header File/Library to use basic Function.
 #include <stdio.h>
 
-int main(){
+//Entry Point of C Program
+int main(void){
 
+    //printf Function is defined in stdio.h header file 
+    //It is use to print output in console
     printf("Hello_World");
 
+    
 
+
+
+    /*Return 0 means it send sucess exit status to operating system
+      Other than 0 any other Integer send Error status to operating system*/
     return 0;
 }
